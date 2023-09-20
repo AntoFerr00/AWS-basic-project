bcddc0dddd52949c06c242ad856c5e2d48d17cdb
tony002009 <tony002009@hotmail.it>
2023-09-20 16:47:58 +0200

    last commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_WV3OdGCV9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4rhkjc5hkbaerti31tc4p0k44g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://udw4paog14.execute-api.eu-north-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
